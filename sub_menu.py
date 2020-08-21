aac23cbadf3d8c44bd3c79e781f654c6c552fbaa
anishmoktan <anish.tmngo@gmail.com>
2020-08-21 18:32:25 -0400

    before class

--- sub_menu.py
+++ sub_menu.py
@@ -1,14 +1,26 @@
 #Shows what the user can have access to when they sign in
 
 from account import Account
+import json
+import requests
+import os
 
-class Sub_Menu():
-    def __init__(self,account):
-        self.account=account
-
-    
-
-
+class search_image:
+  def __init__(self,image):
+      image = image.lower()
+      base_url  = "https://api.pexels.com/v1/search"
+      key = os.getenv('api_key')
+      query_params = {"function": "TIME_SERIES_INTRADAY" , "symbol": stock_symbol, "interval": "5min", "apikey": key}
+      response = requests.get(base_url, query_params)
+      if response.status_code == 200:
+          print('\nWe were able to find the value of the stock!')
+      elif response.status_code == 404:
+          print('Sorry we could not find the stock you were looking for')
+          return(self.search_stock)
+      data = response.json()
+      last_referesh = data['Meta Data']["3. Last Refreshed"]
+      self.value= float(data["Time Series (5min)"][last_referesh]["1. open"])
+      print("The market price for" + " " + stock_symbol + " "+ "stock is currently $" + str(self.value) + "\n")
 
 class Sub_Menu:
     
